3fb25387d98eb649ec55b74a99bdfe664aa6b8b4
Bobby Gagnon <robertgagnon726@gmail.com>
2022-09-11 14:11:02 -0400

    updated publish script

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,58 +1,48 @@
-const exec = require("child_process").exec;
+const {execSync} = require("child_process");
 const fs = require("fs");
-
-// Check for local changes
-// Commit and push the version number
-// Run npm publish
 
 console.log("Checking for local changes...");
 
-exec("git status", (error, stdout, stderr) => {
-  if (error) {
-    console.log(`error: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.log(`stderr: ${stderr}`);
-    return;
-  }
+let status = execSync("git status", "utf8");
+status = status.toString();
 
-  const isAhead = stdout.includes("ahead");
-  const hasDiff = stdout.includes("diff");
-  if (isAhead) {
-    console.error("Error - You have unpushed changes");
-    return;
-  }
-  if (hasDiff) {
-    console.error("Error - You have local changes");
-    return;
-  }
+const isAhead = status.includes("ahead");
+const hasDiff = status.includes("diff") || status.includes("modified");
+if (isAhead) {
+  console.error("Error - You have unpushed changes");
+  return;
+}
+if (hasDiff) {
+  console.error("Error - You have local changes");
+  return;
+}
 
-  console.log("Check for local changes completed.");
+console.log("Incrementing version number...");
+const content = fs.readFileSync("package.json", "utf8");
 
-  console.log("Incrementing version number...");
-  fs.readFile("package.json", function (err, content) {
-    if (err) throw err;
-    const package = JSON.parse(content);
-    const version = package.version;
-    const versionArr = version.split(".");
-    const patchVersion = versionArr[versionArr.length - 1];
-    const majorVersion = versionArr[0];
-    const minorVersion = versionArr[1];
-    const newPatchVersion = new Number(patchVersion) + 1;
-    const newVersion = `${majorVersion}.${minorVersion}.${newPatchVersion}`;
-    const updatedPackage = {...package};
-    updatedPackage.version = newVersion;
-    fs.writeFile(
-      "package.json",
-      JSON.stringify(updatedPackage),
-      function (err) {
-        if (err) throw err;
-        console.log("test");
-      }
-    );
-  });
+const package = JSON.parse(content);
+const version = package.version;
+const versionArr = version.split(".");
+const patchVersion = versionArr[versionArr.length - 1];
+const majorVersion = versionArr[0];
+const minorVersion = versionArr[1];
+const newPatchVersion = new Number(patchVersion) + 1;
+const newVersion = `${majorVersion}.${minorVersion}.${newPatchVersion}`;
+const updatedPackage = {...package};
+updatedPackage.version = newVersion;
 
-  exec("npm run prettier-package-json");
-  exec("git add ./package.json");
-});
+fs.writeFileSync("package.json", JSON.stringify(updatedPackage));
+
+console.log("Start formatting package.json...");
+execSync("npm run prettier-package-json");
+console.log("Finish formatting package.json");
+
+console.log("Push up package.json changes...");
+execSync("git add package.json");
+execSync("git commit -m 'incremented version'");
+execSync("git push");
+console.log("Finished pushing up package.json changes...");
+
+// console.log("Starting npm publish...");
+// execSync("npm publish");
+// console.log("npm publish finished");
