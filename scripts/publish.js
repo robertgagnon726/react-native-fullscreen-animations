c4de0fb3bad03092081a21a8987e6fff4b789b23
Bobby Gagnon <robertgagnon726@gmail.com>
2022-09-11 13:44:08 -0400

    build updates

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,10 +1,11 @@
 const exec = require("child_process").exec;
+const fs = require("fs");
 
-// Make sure there are no unstaged or staged changes
-// Make sure that there are no unpushed changes
-// Increment the version number
+// Check for local changes
 // Commit and push the version number
 // Run npm publish
+
+console.log("Checking for local changes...");
 
 exec("git status", (error, stdout, stderr) => {
   if (error) {
@@ -15,8 +16,43 @@ exec("git status", (error, stdout, stderr) => {
     console.log(`stderr: ${stderr}`);
     return;
   }
-  console.log(stdout.includes("ahead"));
-  console.log(stdout.includes("diff"));
-  console.log(stdout.includes("+"));
-  console.log(`stdout: ${stdout}`);
+
+  const isAhead = stdout.includes("ahead");
+  const hasDiff = stdout.includes("diff");
+  if (isAhead) {
+    console.error("Error - You have unpushed changes");
+    return;
+  }
+  if (hasDiff) {
+    console.error("Error - You have local changes");
+    return;
+  }
+
+  console.log("Check for local changes completed.");
+
+  console.log("Incrementing version number...");
+  fs.readFile("package.json", function (err, content) {
+    if (err) throw err;
+    const package = JSON.parse(content);
+    const version = package.version;
+    const versionArr = version.split(".");
+    const patchVersion = versionArr[versionArr.length - 1];
+    const majorVersion = versionArr[0];
+    const minorVersion = versionArr[1];
+    const newPatchVersion = new Number(patchVersion) + 1;
+    const newVersion = `${majorVersion}.${minorVersion}.${newPatchVersion}`;
+    const updatedPackage = {...package};
+    updatedPackage.version = newVersion;
+    fs.writeFile(
+      "package.json",
+      JSON.stringify(updatedPackage),
+      function (err) {
+        if (err) throw err;
+        console.log("test");
+      }
+    );
+  });
+
+  exec("npm run prettier-package-json");
+  exec("git add ./package.json");
 });
